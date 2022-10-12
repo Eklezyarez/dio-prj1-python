cbc15551f07a9617894556597094ba12dc8bd1d3
Eklezyarez <eklezyarez@gmail.com>
2022-10-12 19:43:55 -0300

    update

--- image_processing/processing/combination.py
+++ image_processing/processing/combination.py
@@ -8,5 +8,11 @@ def find_difference(image1, image2):
     gray_image1 = rgb2gray(image1)
     gray_image2 = rgb2gray(image2)
     (score, difference_image) = structural_similarity(gray_image1, gray_image2, full=true)
-    print("Similarity")
+    print("Similarity of the images:", score)
+    normalized_difference_image = (difference_image-np.min(difference_image))/(np.max(difference_image)-np.min(difference_image))
+    return normalized_difference_image
+
+def transfer_histogram(image1, image2):
+    matched_image = match_histograms(image1, image2, multichannel=True)
+    return matched_image
 
